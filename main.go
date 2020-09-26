2eb2c6e5fae5f1e8ac333ec1279ddd43d3af0e4d
zhouhuan <zhou-huan@139.com>
2020-09-26 21:24:01 +0800

    get the citylist by url

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello World !")
 	fmt.Println("Hello Go world!")
 	fmt.Println("Hello GitHub world!")
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		all, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", all)
+	}
+
 }
